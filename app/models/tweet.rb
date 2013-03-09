8441475e0acec17dc14102aceac94a6f7c2f62a4
Michael Morrissey <mikehmorrissey@gmail.com>
2013-03-09 13:38:49 -0500

    add model before save call back

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,13 @@
 class Tweet < ActiveRecord::Base
   attr_accessible :pic_url, :text, :tweet_id, :user_name
-
+  before_save :parse_tweet
   validates :tweet_id, presence: true, uniqueness: true
+
+   def parse_tweet
+   		t = Twitter.status(tweet_id)
+   		self.user_name = t.from_user
+   		self.text = t.full_text
+   		self.pic_url = t.user.profile_image_url(size = :normal)
+    end
+
 end
