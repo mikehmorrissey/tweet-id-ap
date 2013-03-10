c2cbadf6749f4d8b5adea67bb8aff9904a0fd317
Michael Morrissey <mikehmorrissey@gmail.com>
2013-03-09 22:25:21 -0500

    css styling for show

--- app/custom_validators/tweet_exist_validator.rb
+++ app/custom_validators/tweet_exist_validator.rb
@@ -2,7 +2,7 @@ class TweetExistValidator < ActiveModel::EachValidator
 	def validate_each (object, attribute, value)
 		begin Twitter.status(value)
 		rescue Twitter::Error
-			object.errors[attribute] << "No Tweet exists with given ID"
+			object.errors[attribute] << "must have vailid ID"
 		end
 	end
 end
